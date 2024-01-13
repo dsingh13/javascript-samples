d42bf5acd3fe1a8033059ae92f6caae95f9c2b14
dsingh13 <deepaksingh1981@gmail.com>
2024-01-13 05:46:26 +0000

    Basic 01 JS

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name = "Sam"
+const repoCount = 50
+//console.log(name + repoCount + "Value")
+console.log(`Hello my name is ${name} and my repo count is ${repoCount-1}`)
+
+const gameName = new String('Uncle Sam')
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+const newString = gameName.substring(0, 5)
+console.log(newString)
